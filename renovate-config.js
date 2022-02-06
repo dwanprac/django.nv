88ccc2d732c3cd5dee76eb6e38ad9f5426c4a29b
dwanprac <98000244+dwanprac@users.noreply.github.com>
2022-02-06 12:12:29 +0800

    Update renovate-config.js

--- renovate-config.js
+++ renovate-config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  branchPrefix: 'test-renovate/',
-  username: 'renovate-release',
-  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
   platform: 'github',
-  includeForks: true,
   onboardingConfig:
   {
     extends: ["config:base"]
